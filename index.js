fcc87e849a3836b01e27dcf024d0233a502e9a11
Nicolas Ramirez <nicolas@matizlab.com>
2022-08-14 11:30:15 +0200

    create user endpoint

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const md5 = require('md5')
 require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
+})
 
+const users = []
 
+function createUser(username) {
+  const _id = md5(username + Date.now())
+  return {
+    _id,
+    username,
+  }
+}
+
+app.post('/api/users', (req, res) => {
+  const { username } = req.body
+  const user = createUser(username)
+  console.log(user)
+
+  res.send(user)
+})
 
 
 
